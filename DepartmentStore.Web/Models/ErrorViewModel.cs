d05c96ae9b7a1b749e573e22fc098e28dbf8f881
QuachThang <QuachThang@DESKTOP-4924MRA>
2019-05-24 22:28:51 +0700

    ThangQV add structure project

---
+++ DepartmentStore.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DepartmentStore.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
